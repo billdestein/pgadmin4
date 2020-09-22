2caf524e29f472615543ffd21c88fbda46cbf80f
wdestein <bill_destein@intuit.com>
2020-09-22 06:47:51 -0700

    Redshift does not support client_min_messages

--- web/pgadmin/utils/driver/psycopg2/connection.py
+++ web/pgadmin/utils/driver/psycopg2/connection.py
@@ -430,14 +430,24 @@ class Connection(BaseConnection):
         # Note that we use 'UPDATE pg_settings' for setting bytea_output as a
         # convenience hack for those running on old, unsupported versions of
         # PostgreSQL 'cos we're nice like that.
+
+        # xxx toolbox -- Redshift does not support 'client_min_messages'
         status = self._execute(
             cur,
             "SET DateStyle=ISO; "
-            "SET client_min_messages=notice; "
             "SELECT set_config('bytea_output','hex',false) FROM pg_settings"
             " WHERE name = 'bytea_output'; "
             "SET client_encoding='{0}';".format(postgres_encoding)
         )
+        
+        # status = self._execute(
+        #     cur,
+        #     "SET DateStyle=ISO; "
+        #     "SET client_min_messages=notice; "
+        #     "SELECT set_config('bytea_output','hex',false) FROM pg_settings"
+        #     " WHERE name = 'bytea_output'; "
+        #     "SET client_encoding='{0}';".format(postgres_encoding)
+        # )
 
         if status is not None:
             self.conn.close()
