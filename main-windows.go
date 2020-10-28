b89d9863f3d4ede0f837affc64c58facbf3e520e
Bill DeStein <billdestein@gmail.com>
2020-10-28 05:35:03 -0500

    More work

--- main-windows.go
+++ main-windows.go
@@ -6,10 +6,9 @@ package main
 //
 //     - Compile the Python code using qmake.
 //     - Compile the web assets using yarn.
-//     - Create a tarball and put in ~/.toolbox-tarballs
+//     - Create a tarball and put in ~/toolbox-tarballs
 //
-// The toolbox build process will copy the tarball from ~/.toolbox-tarballs into the release
-// tarball.
+// At this point, we need to manually upload the tarball to the toolbox-at-large S3 bucket.
 //
 // This program also assumes that it is running in a Python >= 3.4 virtual environment.
 //
@@ -42,7 +41,7 @@ func (this Builder) build() {
   currentUser, _ := user.Current();
   homeDir := currentUser.HomeDir    
   tarballDir := path.Join(homeDir, "toolbox-tarballs")
-  tarballFilepath := path.Join(tarballDir, "toolbox-pgadmin4.tgz")
+  tarballFilepath := path.Join(tarballDir, "toolbox-pgadmin4-windows.tgz")
 
   // Find the qmake executable
   qmakeExecutable, err := exec.LookPath("qmake")
@@ -148,58 +147,6 @@ func (this Builder) build() {
     fmt.Printf("error running command '%s' '%s'. %s\n", command.String(), output, err.Error())
     os.Exit(1)
   }
-
-
-  // // Build the web assets
-  // webDir := path.Join(repoDir, "web")
-
-  // // Exec 'yarn install'
-  // command = fmt.Sprintf("yarn install")
-  // fmt.Printf("command: '%s'\n", command)
-  // os.Chdir(webDir)
-  // output, err = exec.Command("bash", "-c", command).Output()
-  // os.Chdir(repoDir)
-  // if err != nil {
-  //   fmt.Printf("Failed to execute command '%s'. %s %s\n", command, output, err.Error())
-  //   os.Exit(1)
-  // }
-
-  // // Exec 'yarn run build'
-  // command = fmt.Sprintf("yarn run bundle")
-  // fmt.Printf("command: '%s'\n", command)
-  // os.Chdir(webDir)
-  // output, err = exec.Command("bash", "-c", command).Output()
-  // os.Chdir(repoDir)
-  // if err != nil {
-  //   fmt.Printf("Failed to execute command '%s'. %s\n", command, output, err.Error())
-  //   os.Exit(1)
-  // }
-
-  // // Create the tarball directory if it does not exist
-  // _, err = os.Stat(tarballDir)
-  // if os.IsNotExist(err) {
-  //   os.MkdirAll(tarballDir, 0700)
-  // }
-
-  // // If the tarball exists, remove it
-  // _, err = os.Stat(tarballFilepath)
-  // if !os.IsNotExist(err) {
-  //   os.RemoveAll(tarballFilepath)
-  //   if err != nil {
-  //     fmt.Printf("Error removing tarball. %s\n", err.Error())
-  //     os.Exit(1)
-  //   }
-  // }
-
-  // // Tar the pgadmin directory
-  // parentDir := filepath.Dir(repoDir)
-  // command = fmt.Sprintf("tar -C  %s -czf %s toolbox-pgadmin4", parentDir, tarballFilepath)
-  // fmt.Printf("command: '%s'\n", command)
-  // _, err = exec.Command("bash", "-c", command).Output()
-  // if err != nil {
-  //   fmt.Printf("error tarring pgadmin. %s\n", err.Error())
-  //   os.Exit(1)
-  // }
 }
 
 func main() {
